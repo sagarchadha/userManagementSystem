34c7947653debcda40d02a378f08070add22ca21
Sagar Chadha <sagarchadha@gmail.com>
2021-05-20 18:47:27 -0400

    test: create test to ensure application loads

--- mobile-app-ws/src/test/java/com/appdeveloperblog/app/ws/MobileAppWsApplicationTests.java
+++ mobile-app-ws/src/test/java/com/appdeveloperblog/app/ws/MobileAppWsApplicationTests.java
@@ -1,13 +1,27 @@
 package com.appdeveloperblog.app.ws;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.appdeveloperblog.app.ws.service.UserService;
+import com.appdeveloperblog.app.ws.ui.controller.UserController;
 
 @SpringBootTest
 class MobileAppWsApplicationTests {
-
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserController userController;
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(userService).isNotNull();
+		assertThat(userController).isNotNull();
 	}
 
 }
