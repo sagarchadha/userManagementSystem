52ba3f77aee16dc2a078c322d8a43621ea452e83
Sagar Chadha <sagarchadha@gmail.com>
2021-06-01 23:40:45 -0400

    feat: create address service with base implementation

---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/service/AddressService.java
@@ -0,0 +1,9 @@
+package com.appdeveloperblog.app.ws.service;
+
+import java.util.List;
+
+import com.appdeveloperblog.app.ws.shared.dto.AddressDto;
+
+public interface AddressService {
+	List<AddressDto> getAddressList(String userId);
+}
---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/service/impl/AddressServiceImpl.java
@@ -0,0 +1,16 @@
+package com.appdeveloperblog.app.ws.service.impl;
+
+import java.util.List;
+
+import com.appdeveloperblog.app.ws.service.AddressService;
+import com.appdeveloperblog.app.ws.shared.dto.AddressDto;
+
+public class AddressServiceImpl implements AddressService {
+
+	@Override
+	public List<AddressDto> getAddressList(String userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
