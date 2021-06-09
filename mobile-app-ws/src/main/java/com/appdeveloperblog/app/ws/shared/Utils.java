00aee004b0de672aa7c3b2c645c850acd6f89395
Sagar Chadha <sagarchadha@gmail.com>
2021-06-08 23:24:34 -0400

    feat: create email token utility functions

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/shared/Utils.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/shared/Utils.java
@@ -1,13 +1,36 @@
 package com.appdeveloperblog.app.ws.shared;
 
+import java.util.Date;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
+import com.appdeveloperblog.app.ws.security.SecurityConstants;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
 public class Utils {
-	
+
 	public String randomString() {
 		return UUID.randomUUID().toString();
+	}
+
+	public String generateEmailVerificationToken(String userId) {
+		String token = Jwts.builder().setSubject(userId)
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.getTokenSecret()).compact();
+		return token;
+	}
+
+	public static boolean tokenExpiredStatus(String token) {
+		Claims claims = Jwts.parser().setSigningKey(SecurityConstants.getTokenSecret()).parseClaimsJws(token).getBody();
+
+		Date tokenExpirationDate = claims.getExpiration();
+		Date todayDate = new Date();
+
+		return tokenExpirationDate.before(todayDate);
 	}
 }
