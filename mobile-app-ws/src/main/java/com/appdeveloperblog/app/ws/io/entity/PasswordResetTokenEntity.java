6499d01cc5ac30f58f2e62f9ad7748027a12557c
Sagar Chadha <sagarchadha@gmail.com>
2021-06-18 20:56:36 -0400

    feat: create password reset token entity and repository

---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/io/entity/PasswordResetTokenEntity.java
@@ -0,0 +1,48 @@
+package com.appdeveloperblog.app.ws.io.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+@Entity(name = "password_reset_tokens")
+public class PasswordResetTokenEntity implements Serializable {
+
+	private static final long serialVersionUID = 2066281571727505643L;
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	private String token;
+
+	@JoinColumn(name = "users_id")
+	private UserEntity userDetails;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public UserEntity getUserDetails() {
+		return userDetails;
+	}
+
+	public void setUserDetails(UserEntity userDetails) {
+		this.userDetails = userDetails;
+	}
+
+}
