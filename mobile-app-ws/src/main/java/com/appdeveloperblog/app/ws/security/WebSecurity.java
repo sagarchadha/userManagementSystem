f09e3666a67126ce1e7f23c3f4e716b1e7c6a4e3
Sagar Chadha <sagarchadha@gmail.com>
2021-06-19 18:35:51 -0400

    fix: make password reset endpoint public

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/security/WebSecurity.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/security/WebSecurity.java
@@ -25,6 +25,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
 				.permitAll().antMatchers(HttpMethod.GET, SecurityConstants.EMAIL_VERIFICATION_URL).permitAll()
+				.antMatchers(HttpMethod.POST, SecurityConstants.PASSWORD_RESET_URL).permitAll()
 				.anyRequest().authenticated().and().addFilter(getAuthenticationFilter())
 				.addFilter(new AuthorizationFilter(authenticationManager())).sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
