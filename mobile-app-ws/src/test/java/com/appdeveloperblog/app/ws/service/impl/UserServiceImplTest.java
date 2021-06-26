aa9c6380c2e61e6f21eda70a7a4eecf71cc20003
Sagar Chadha <sagarchadha@gmail.com>
2021-06-26 00:51:03 -0400

    test: create tests for create user function

--- mobile-app-ws/src/test/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
+++ mobile-app-ws/src/test/java/com/appdeveloperblog/app/ws/service/impl/UserServiceImplTest.java
@@ -3,8 +3,12 @@ package com.appdeveloperblog.app.ws.service.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,12 +16,14 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.appdeveloperblog.app.ws.exceptions.UserServiceException;
 import com.appdeveloperblog.app.ws.io.entity.UserEntity;
 import com.appdeveloperblog.app.ws.io.repositories.UserRepository;
 import com.appdeveloperblog.app.ws.shared.AmazonSES;
@@ -98,12 +104,44 @@ class UserServiceImplTest {
 	}
 
 	@Test
-	void testgetUserNotFound() {
+	void testGetUserNotFound() {
 		when(userRepository.findByEmail(anyString())).thenReturn(null);
 
 		assertThrows(UsernameNotFoundException.class, () -> {
 			userService.getUser(userDto.getEmail());
 		});
+	}
+
+	@Test
+	void testCreateUser() {
+		UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+		when(userRepository.findByEmail(anyString())).thenReturn(null);
+		when(utils.randomString()).thenReturn(RandomStringUtils.randomAlphanumeric(10));
+		when(bCryptPasswordEncoder.encode(anyString())).thenReturn(RandomStringUtils.randomAlphanumeric(10));
+		when(userRepository.save(any(UserEntity.class))).thenReturn(userEntity);
+		Mockito.doNothing().when(amazonSES).verifyEmail(any(UserDto.class));
+
+		List<AddressDto> addressList = new ArrayList<>();
+		addressList.add(addressDto);
+		userDto.setAddresses(addressList);
+
+		UserDto userDtoResult = userService.createUser(userDto);
+		assertNotNull(userDtoResult);
+
+		assertEquals(userDto.getFirstName(), userDtoResult.getFirstName());
+		assertEquals(userDto.getLastName(), userDtoResult.getLastName());
+		assertEquals(userDto.getEmail(), userDtoResult.getEmail());
+		assertEquals(userDto.getEncryptedPassword(), userDtoResult.getEncryptedPassword());
+	}
+
+	@Test
+	void testCreateUserExits() {
+		UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+		when(userRepository.findByEmail(anyString())).thenReturn(userEntity);
+		assertThrows(UserServiceException.class, () -> {
+			userService.createUser(userDto);
+		});
+
 	}
 
 }
