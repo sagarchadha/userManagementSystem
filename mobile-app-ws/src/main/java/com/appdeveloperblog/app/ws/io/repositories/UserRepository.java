651e9a616ef558fb164938733da6e6fca224ce4b
Sagar Chadha <sagarchadha@gmail.com>
2021-07-04 22:05:38 -0400

    feat: add custom query to search for first and last name of users

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/io/repositories/UserRepository.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/io/repositories/UserRepository.java
@@ -1,6 +1,10 @@
 package com.appdeveloperblog.app.ws.io.repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.appdeveloperblog.app.ws.io.entity.UserEntity;
@@ -11,5 +15,8 @@ public interface UserRepository extends PagingAndSortingRepository<UserEntity, L
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
 	UserEntity findByEmailVerificationToken(String token);
+	
+	@Query(value="select user.first_name, user.last_name from Users user where user.first_name LIKE %:name% or user.last_name LIKE %:name%", nativeQuery = true)
+	List<Object[]> findUserFirstNameLastNameByName(@Param("name") String name);
 
 }
