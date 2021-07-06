e241cb11f611c1401e56907c78608fedc9300fa0
Sagar Chadha <sagarchadha@gmail.com>
2021-07-05 23:18:20 -0400

    feat: create config and add dependencies for swagger

---
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.appdeveloperblog.app.ws;
+
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+	public Docket apiDocket() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.app.ws")).paths(PathSelectors.any())
+				.build();
+	}
+}
