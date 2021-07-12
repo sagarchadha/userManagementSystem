26104f47c1f0b91468ea07e8e7873d07dc17d8e7
Sagar Chadha <sagarchadha@gmail.com>
2021-07-11 23:26:15 -0400

    feat: add app and contact details in swagger

--- mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/SwaggerConfig.java
+++ mobile-app-ws/src/main/java/com/appdeveloperblog/app/ws/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.appdeveloperblog.app.ws;
 
+import java.util.ArrayList;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,10 +16,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	
+	Contact contact = new Contact("Sagar Chadha", null, "sagarchadha@gmail.com");
+
+	ApiInfo apiInfo = new ApiInfo("User Management RESTful Web Service Documention",
+			"This page documents all of the REST endpoints for a user management service.", "1.0", null, contact,
+			"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0", new ArrayList<>());
 
 	@Bean
 	public Docket apiDocket() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo).select()
 				.apis(RequestHandlerSelectors.basePackage("com.appdeveloperblog.app.ws")).paths(PathSelectors.any())
 				.build();
 	}
